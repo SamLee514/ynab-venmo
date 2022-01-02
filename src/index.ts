8b5ba6a179991a961042d04ba8476e451ef0f7f2
Samuel Lee <sam@samlee.dev>
2022-01-02 12:15:12 -0500

    put it all in an async function

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-import { SaveTransaction } from "ynab";
 import { EmailListener } from "./emailListener";
 import { parseVenmoEmail } from "./utils";
 import { YnabVenmo } from "./ynabVenmo";
-import {
-  CreateTransactionNoAccountID,
-  UpdateTransactionFields,
-} from "./ynabVenmo";
 
-const connectionConfig = {
-  user: process.env.EMAIL_USERNAME || "",
-  password: process.env.EMAIL_PASSWORD || "",
-  host: process.env.EMAIL_HOST || "",
-  port: parseInt(process.env.EMAIL_PORT || ""),
-  tls: false,
+const main = async () => {
+  const connectionConfig = {
+    user: process.env.EMAIL_USERNAME || "",
+    password: process.env.EMAIL_PASSWORD || "",
+    host: process.env.EMAIL_HOST || "",
+    port: parseInt(process.env.EMAIL_PORT || ""),
+    tls: false,
+  };
+
+  const ynabVenmo = new YnabVenmo(process.env.YNAB_TOKEN || "", "Venmo");
+  await ynabVenmo.init();
+
+  const emailListener = new EmailListener(connectionConfig, async (parsed) => {
+    const transactionInfo = parseVenmoEmail(parsed);
+    switch (transactionInfo.type) {
+      case "CREATE":
+        await ynabVenmo.createTransaction(transactionInfo);
+        break;
+      case "UPDATE":
+        await ynabVenmo.updateTransaction(transactionInfo);
+        break;
+    }
+  });
+
+  emailListener.start();
 };
 
-const ynabVenmo = new YnabVenmo(process.env.YNAB_TOKEN || "", "Venmo");
-
-const emailListener = new EmailListener(connectionConfig, async (parsed) => {
-  const transactionInfo = parseVenmoEmail(parsed);
-  await ynabVenmo.init(); // TODO don't do multiple inits
-  switch (transactionInfo.type) {
-    case "CREATE":
-      await ynabVenmo.createTransaction(transactionInfo);
-      break;
-    case "UPDATE":
-      await ynabVenmo.updateTransaction(transactionInfo);
-      break;
-  }
-});
-
-emailListener.start();
+main();
